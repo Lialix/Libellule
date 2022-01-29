eba91f667c002b3a416ca49ded468b7367115618
LiAlix <li.alix.pro@gmail.com>
2022-01-29 17:35:17 +0100

    cp of libellule

---
+++ chat/views.py
@@ -0,0 +1,13 @@
+from re import template
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+
+class IndexView(generic.TemplateView):
+    template_name = "chat/index.html"
+
+class RoomView(generic.TemplateView):
+    template_name = "chat/room.html"
+    room_name: "room_name"
